662f434b1d255fd96d8dc14957eecda53328dc62
Fahim Faisal Sifat <81096504+Sifat4620@users.noreply.github.com>
2022-03-26 22:36:48 +0600

    Add files via upload

---
+++ toxic mafia 4.php
@@ -0,0 +1,193 @@
+<html>
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=UTF-8">
+<title>fucked</title>
+<link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono" rel=stylesheet type="text/css">
+<link rel=stylesheet href=jquery.countdown.css>
+<style>
+body,td,th {
+	font-family: Source Code Pro,Courier New,Courier,monospace;
+	font-weight: 500;
+	font-size: 100%;
+	text-align:center;
+	background-color: #000;
+	color: #fff;
+}
+footer {
+	margin-top:300px;
+    bottom: 15px;
+    right: 15px;
+    left: 15px;
+    z-index: 20;
+}
+footer .footer-links {
+	text-align:center;
+}
+footer .footer-links a,footer .footer-links span {
+	cursor:pointer;
+	color:#666;
+	font-size:7px;
+	margin-right:6px;
+}
+footer .footer-links a:hover,footer .footer-links span:hover {
+	color:#fff;
+}
+footer .nbc-copy {
+	margin-top:3px;
+	text-align:center;
+	color:#555;
+	font-size:7px;
+}
+footer .nbc-copy:hover {
+	color:#fff;
+}
+p.large {
+	font-size: 20px;
+	color: #00ff00;
+	font-weight: bold;
+	text-transform: uppercase;
+}
+#defaultCountdown {
+	color: #000;
+}
+@media screen and (min-width:32em) {
+	footer {
+		text-align:center;
+	}
+	footer .footer-links,footer .nbc-copy {
+		display:inline-block;
+	}
+	footer .footer-links span a,footer .nbc-copy {
+		font-size:10px
+	}
+	footer:before {
+		content:'';
+		height:32px;
+		left:0;
+		right:0;
+		top:0;
+		z-index:-1;
+	}
+}
+@media screen and (min-width:64em) {
+	footer {
+		float:right;
+	}
+}
+#defaultCountdown { width: 50%; height: 9vw; }
+#defaultCountdown .countdown-row {
+	font-size: 3vw;
+}
+@media screen and (min-width:1200px) {
+	#defaultCountdown { width: 570px; height: 110px; }
+	#defaultCountdown .countdown-row {
+		font-size: 36px;
+	}
+}
+.over {
+	position: relative;
+	background-color: black;
+	padding: 20px;
+	padding-bottom: 50px;
+	text-align: left;
+	color:white;
+}
+.author {
+	font-weight: bold;
+	position: absolute;
+	right: 20px;
+	bottom: 20px;
+}
+@media screen and (max-width:530px) {
+	#defaultCountdown { width: 90%; }
+}
+</style>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src=jquery.plugin.js></script>
+<script src=jquery.countdown.js></script>
+<script src="//assets.adobedtm.com/5ed6cf59538a2045d9fe4799a3f70da302c83d5a/satelliteLib-24e397569ca85a58d3963043838c6696e85c5e9a.js"></script>
+<script language=Javascript1.2>
+ 
+var mymessage = "Fuck off paster.";
+ 
+function rtclickcheck(keyp){
+ 
+  if (navigator.appName == "Netscape" && keyp.which == 3) {
+ 
+    alert(mymessage);
+ 
+    return false;
+ 
+  }
+ 
+  if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) {
+ 
+    alert(mymessage);
+ 
+    return false;
+ 
+  }
+ 
+}
+ 
+document.onmousedown = rtclickcheck
+ 
+<!-- Disable
+ 
+function disableselect(e){
+ 
+return false
+ 
+}
+ 
+function reEnable(){
+ 
+return true
+ 
+}
+ 
+//if IE4+
+ 
+document.onselectstart=new Function ("return false")
+ 
+document.oncontextmenu=new Function ("return false")
+ 
+//if NS6
+ 
+if (window.sidebar){
+ 
+document.onmousedown=disableselect
+ 
+document.onclick=reEnable
+ 
+}
+ 
+//-->
+ 
+</script>
+</head>
+<body>
+<p><img src="http://s16.postimg.org/4hbdh7ygl/mask.gif" style="width:100%;max-width:700px; " border=0 />
+<p class=large>This is our world now. The world of the electron and the switch; the beauty of the baud. We exist without nationality, skin color, or religious bias. You wage wars, murder, cheat, lie to us and try to make us believe it's for our own good, yet we're the criminals. 
+<br>
+Yes, I am a criminal. My crime is that of curiosity. I am a hacker, and this is my manifesto." Huh? Right? Manifesto? "You may stop me, but you can't stop us all.<br>
+<br>You wanted to know who I am you little shit? Well, let me explain the New World Order. Governments and corporations need people like you and me. 
+<br>We are Samurai... the Keyboard Cowboys... and all those other people who have no idea what's going on are the cattle... Moooo.<br><br>This is the end, my friend. Thank you for calling.</p><quote>This is the end, my friend. Thank you for calling.
+</quote></p><br>
+<p>UnknownSelfie,<b>It's scary when a stranger controls you.</b></p><br>
+<p>
+<script type="text/javascript">
+//<![CDATA[
+(function() {
+var _analytics_scr = document.createElement('script');
+_analytics_scr.type = 'text/javascript'; _analytics_scr.async = true; _analytics_scr.src = '/_Incapsula_Resource?SWJIYLWA=2977d8d74f63d7f8fedbea018b7a1d05,cf352368b953421da63f9283d333e0e9&ns=1';
+var _analytics_elem = document.getElementsByTagName('script')[0]; _analytics_elem.parentNode.insertBefore(_analytics_scr, _analytics_elem);
+})();
+// ]]>
+</script></body>
+</html>
+<script>
+        var music = ['music1.mp3', 'music2.mp3', 'music3.mp3'];
+        var rand = music[Math.floor(Math.random() * music.length)];
+        document.write("<audio autoplay='autoplay'>"+"<source src='"+rand+"'"+"type='audio/mpeg'>"+"</audio>");
+    </script>
